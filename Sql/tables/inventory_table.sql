d847abd4ff879f92eb198604ab4e8288d22b14b9
krazyforce <40051025+krazyforce@users.noreply.github.com>
2019-11-17 21:01:30 -0600

    Update inventory SQL

--- Sql/tables/inventory_table.sql
+++ Sql/tables/inventory_table.sql
@@ -3,14 +3,17 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2019 at 03:42 AM
+-- Generation Time: Nov 18, 2019 at 04:00 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,38 +31,47 @@ SET time_zone = "+00:00";
 -- Table structure for table `inventory`
 --
 
-CREATE TABLE `inventory` (
-  `id` int(255) NOT NULL,
-  `itemName` varchar(255) NOT NULL,
+CREATE TABLE `inventory`
+(
+  `id` int
+(255) NOT NULL,
+  `itemName` varchar
+(255) NOT NULL,
   `price` float NOT NULL,
-  `department` varchar(13) NOT NULL
+  `department` varchar
+(13) NOT NULL,
+  `pictureID` varchar
+(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `inventory`
 --
 
-INSERT INTO `inventory` (`id`, `itemName`, `price`, `department`) VALUES
-(1, 'Pato Tang', 40, 'Men'),
-(2, 'Honey Nilo Croc', 50, 'Men'),
-(3, 'Papaya Star', 50, 'Men'),
-(4, 'Braided', 50, 'Men'),
-(5, 'Longhorn', 50, 'Men'),
-(6, 'Cactus Brown', 45, 'Women'),
-(7, 'Crystal Flower', 40, 'Women'),
-(8, 'Honey Flower', 40, 'Women'),
-(9, 'Flower Honey Brown', 45, 'Women'),
-(10, 'Hojitas', 40, 'Women'),
-(11, 'Honey Crocodile', 35, 'Juvenile'),
-(12, 'Crazy Papaya', 35, 'Juvenile'),
-(13, 'Pirarucu Cognac', 30, 'Juvenile'),
-(14, 'Star Cognac', 30, 'Juvenile'),
-(15, 'Texas Cognac', 30, 'Juvenile'),
-(16, 'Texas Newborn', 20, 'Toddler'),
-(17, 'Pink Newborn', 20, 'Toddler'),
-(18, 'Pink Flower Toddler', 25, 'Toddler'),
-(19, 'Longhorn Toddler', 25, 'Toddler'),
-(20, 'Texas Toddler\r\n', 30, 'Toddler');
+INSERT INTO `inventory` (`
+id`,
+`itemName
+`, `price`, `department`, `pictureID`) VALUES
+(1, 'Pato Tang', 40, 'Men', ''),
+(2, 'Honey Nilo Croc', 50, 'Men', ''),
+(3, 'Papaya Star', 50, 'Men', ''),
+(4, 'Braided', 50, 'Men', ''),
+(5, 'Longhorn', 50, 'Men', ''),
+(6, 'Cactus Brown', 45, 'Women', ''),
+(7, 'Crystal Flower', 40, 'Women', ''),
+(8, 'Honey Flower', 40, 'Women', ''),
+(9, 'Flower Honey Brown', 45, 'Women', ''),
+(10, 'Hojitas', 40, 'Women', ''),
+(11, 'Honey Crocodile', 35, 'Juvenile', ''),
+(12, 'Crazy Papaya', 35, 'Juvenile', ''),
+(13, 'Pirarucu Cognac', 30, 'Juvenile', ''),
+(14, 'Star Cognac', 30, 'Juvenile', ''),
+(15, 'Texas Cognac', 30, 'Juvenile', ''),
+(16, 'Texas Newborn', 20, 'Toddler', ''),
+(17, 'Pink Newborn', 20, 'Toddler', ''),
+(18, 'Pink Flower Toddler', 25, 'Toddler', ''),
+(19, 'Longhorn Toddler', 25, 'Toddler', ''),
+(20, 'Texas Toddler\r\n', 30, 'Toddler', '');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +81,8 @@ INSERT INTO `inventory` (`id`, `itemName`, `price`, `department`) VALUES
 -- Indexes for table `inventory`
 --
 ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -79,7 +92,8 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int
+(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
